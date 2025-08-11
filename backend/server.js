93ffbdcdda0aa37b3c5d2fbd484c4cda7c717c89
NobleWolf <dolfshungube@gmail.com>
2025-08-11 22:26:24 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,17 +13,14 @@ app.get("/api", (req, res) => {
 
 
 
-app.use(express.static(path.join(__dirname,"../frontend/dist")));
+app.use(express.static(path.join(__dirname,"frontend/dist")));
 
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname,"frontend/dist/index.html"));
 });
 
 
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
-});
 
 
 app.listen(port, "0.0.0.0", () => {
