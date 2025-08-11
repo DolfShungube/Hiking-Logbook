a527ab1d81a59f60ffc5eafd60ce4b323bceb300
NobleWolf <dolfshungube@gmail.com>
2025-08-11 23:40:34 +0200

    Update unbundled frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,12 @@ app.get("/api", (req, res) => {
 
 
 
-app.use(express.static(path.join(__dirname,"frontend/dist")));
+// app.use(express.static(path.join(__dirname,"frontend/dist")));
 
 
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname,"frontend/dist/index.html"));
-});
+// app.get(/.*/, (req, res) => {
+//   res.sendFile(path.join(__dirname,"frontend/dist/index.html"));
+// });
 
 
 
