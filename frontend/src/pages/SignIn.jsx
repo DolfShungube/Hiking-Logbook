2909f62f7a8a3adfcc4e8cad8f5eb19bf1eed541
DolfShungube <dolfshungube@gmail.com>
2025-08-15 17:36:47 +0200

    update: added email athentiacation

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import SignInImage from '../assets/SignIn3.jpg';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { UserAuth } from "../context/AuthContext";
+import Modal from "../components/modal";
 
 const SignIn = () => {
+
+const [email,setEmail]=useState("")
+const [password,setPassword]=useState("")
+const [error,setError]=useState("")
+const [loading,setLoading]=useState("")
+const [showModal,setShowModal]= useState(false)
+
+
+const {session,signInUser,signOutUser}= UserAuth()
+const navigate=useNavigate()
+//console.log(session)
+
+// if(session){
+//   signOutUser()  // for testing
+// }
+
+const handleSignIn= async (e)=>{
+
+
+
+    e.preventDefault()
+    setLoading(true)
+    try{
+        console.log("start")
+      const result= await signInUser(email,password);
+      //console.log(result)
+
+      if(result){
+        // console.log("we have res")
+        // console.log("result.data.error:", result.data);
+
+        if(result.data && result.data.erro){
+          
+                  setError(result.data.error)
+                  setShowModal(true)
+
+                  console.log(result)
+        }else{
+         console.log("dasg") 
+         navigate('/dashboard') //......................................................................
+         // remember to add logout authcontext to dashboard
+        }
+
+
+
+      }
+    }catch(error){
+
+      setError("an arror occured")
+
+
+    }finally{
+      setLoading(false)
+    }
+
+}
+
+
   return (
     <div
       className="min-h-screen bg-cover bg-center flex justify-between items-start text-black px-10"
@@ -17,13 +78,16 @@ const SignIn = () => {
       <div className="absolute top-50 right-50">
         <div className="bg-white p-10 rounded-2xl shadow-2xl max-w-md w-full bg-opacity-90">
           <h2 className="text-3xl font-semibold text-center mb-15">LogIn</h2>
-          <form>
+          { showModal && <Modal heading="ERROR" message={error} setShowModal={setShowModal}/>}
+          <form onSubmit={handleSignIn}>
             <input
+            onChange={(e)=>setEmail(e.target.value)}
               type="email"
               placeholder="Email"
               className="w-full mb-4 p-3 border border-gray-600 rounded-lg"
             />
             <input
+            onChange={(e)=>setPassword(e.target.value)}
               type="password"
               placeholder="Password"
               className="w-full mb-4 p-3 border border-gray-600 rounded-lg"
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,8 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import image from '/src/assets/SignUp1.jpg'
+import { UserAuth } from "../context/AuthContext";
+import Modal from "../components/modal";
 
 const SignUp = () => {
+const [email,setEmail]=useState("")
+const [password,setPassword]=useState("")
+const [firstName,setFirstName]=useState("")
+const [lastName,setLastName]=useState("")
+const [error,setError]=useState("")
+const [heading,setHeading]=useState("")
+const [loading,setLoading]=useState("")
+const [showModal,setShowModal]= useState(false)
+
+const {session,signUpNewUser,signInUser}= UserAuth()
+const navigate=useNavigate()
+
+const handleSignUp= async (e)=>{
+
+    e.preventDefault()
+    setLoading(true)
+    try{
+      const result= await signUpNewUser(firstName,lastName,email,password)
+
+      if(result){
+        console.log(result)
+
+        if(result.error){
+        setShowModal(true)
+        setHeading("ERROR !")
+        setError(result.error) // function to show successs          
+        }else{
+        setShowModal(true)
+        setHeading("SUCCESS !")
+        setError("your account has been created") 
+
+        navigate('/login')
+        }// function to show successs
+        
+       //  //.................
+      }
+    }catch(error){
+
+      setError(error.message)
+      setHeading("ERROR !")
+      setShowModal(true)
+
+
+    }finally{
+      setLoading(false)
+    }
+
+}
+
+
+
+
   return (
     <div
       className="min-h-screen bg-cover bg-center relative text-black"
@@ -17,24 +71,28 @@ const SignUp = () => {
       <div className="flex justify-center items-center h-screen">
         <div className="bg-white p-10 rounded-2xl shadow-2xl max-w-md w-full bg-opacity-90">
           <h2 className="text-3xl font-semibold text-center mb-15">Create an account</h2>
-
-          <form>
+           { showModal && <Modal heading={heading} message={error} setShowModal={setShowModal}/>}  
+          <form onSubmit={handleSignUp}>
              <input
+             onChange={(e)=>setFirstName(e.target.value)}
               type="name"
               placeholder="First Name"
               className="w-full mb-5 p-3 border border-gray-600 rounded-lg"
             />
              <input
+             onChange={(e)=>setLastName(e.target.value)}
               type="name"
               placeholder="Last Name"
               className="w-full mb-5 p-3 border border-gray-600 rounded-lg"
             />
             <input
+            onChange={(e)=>setEmail(e.target.value)}
               type="email"
               placeholder="Email"
               className="w-full mb-5 p-3 border border-gray-600 rounded-lg"
             />
             <input
+            onChange={(e)=>setPassword(e.target.value)}
               type="password"
               placeholder="Password"
               className="w-full mb-5 p-3 border border-gray-600 rounded-lg"
