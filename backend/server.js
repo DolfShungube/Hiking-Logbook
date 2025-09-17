af744dd0c235235d113fabe673b83470d9113351
NobleWolf <dolfshungube@gmail.com>
2025-09-17 20:46:48 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,13 @@ app.use(express.json());
 const weatherRouter = require("./endpoints/weatherAPI.js");
 const { inviteFriend, acceptInvite, rejectInvite, getFriends, hikeInviteFriend, acceptHikeInvite, rejectHikeInvite} = require("./friends/friends.controller");
 const { signIn, signInWithGoogle, signUp } = require("./auth/auth.controller");
-const { fetchCompletedHikes, fetchCurrentHike,CreateNewHike,fetchHike, fetchPlannedHikes, editPlannedHike, deletePlannedHike } = require("./hikeData/hikes.controller");
-//const { CreateNewHike } = require("./hikeData/CreateNewHike");
+//CreateNewHike,fetchHike, fetchPlannedHikes, editPlannedHike, deletePlannedHike
+const { fetchCompletedHikes, fetchCurrentHike} = require("./hikeData/hikes.controller");
+const { CreateNewHike } = require("./hikeData/CreateNewHike");
 const { coordinates } = require("./hikeData/distance.controller.js");
 const { fetchUserRoutes } = require("./hikeData/distance.controller.js");
 
-//const { fetchHike, fetchPlannedHikes, editPlannedHike, deletePlannedHike } = require("./hikeData/plannedHikes_details.js");
+const { fetchHike, fetchPlannedHikes, editPlannedHike, deletePlannedHike } = require("./hikeData/plannedHikes_details.js");
 const { fetchUser, getUserByName } = require("./users/users.controller");
 const { getNotes, addNotes, removeNotes } = require("./notes/notes.controller");
 const { getGoals, addGoal, updateGoalStatus, removeGoal } = require("./goals/goals.controller");
